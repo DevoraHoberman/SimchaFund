a69468a37176b801e114d8161fadcd807efbc576
Devora Hoberman <devorahoberman1gmail.com>
2022-03-23 19:26:24 -0400

    Added search feature

--- SimchaFund.Web/wwwroot/js/contributor-deposit.js
+++ SimchaFund.Web/wwwroot/js/contributor-deposit.js
@@ -31,4 +31,22 @@
 
         $('.edit-contrib').modal();        
     })
+
+    $('#search').on('keyup', function () {
+        const text = $(this).val();
+        $("table tr:gt(0)").each(function () {
+            const tr = $(this);
+            const name = tr.find('td:eq(1)').text();
+            if (name.toLowerCase().indexOf(text.toLowerCase()) !== -1) {
+                tr.show();
+            } else {
+                tr.hide();
+            }
+        });
+    });
+
+    $("#clear").on('click', function () {
+        $('#search').val('');
+        $('tr').show();
+    })
 });
